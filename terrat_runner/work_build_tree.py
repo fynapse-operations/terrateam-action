8f8642592bedf08f2ee2b2cfa9a4f4a0fe0d1a33
Ksawery Karwacki <ksawery.karwacki@aptitudesoftware.com>
2025-09-26 13:33:40 +0200

    Support for passing output via file

--- terrat_runner/work_build_tree.py
+++ terrat_runner/work_build_tree.py
@@ -78,7 +78,13 @@ def run(state):
                 })
             if proc.returncode == 0:
                 try:
-                    tree = _cleanup(state.env['TERRATEAM_ROOT'], json.loads(stdout))
+                    tree = {}
+                    if tree_builder['file']:
+                        with open('data.json', 'r') as file:
+                            file_content = json.load(file)
+                            tree = _cleanup(state.env['TERRATEAM_ROOT'], file_content)
+                    else:
+                        tree = _cleanup(state.env['TERRATEAM_ROOT'], json.loads(stdout))
                     requests_retry.put(state.api_base_url + '/v1/work-manifests/' + state.work_token,
                                        json={'files': tree})
                 except json.JSONDecodeError as exn:
